7b6806c6a8513a4115bf14509701a124a280d287
yakir <kashani.yakir@gmail.com>
2025-03-29 16:58:44 +0300

    Added posts

---
+++ app/src/main/java/com/example/woof/Model/NewDog.java
@@ -0,0 +1,84 @@
+package com.example.woof.Model;
+
+public class NewDog {
+    private String breed;
+    private String ownerEmail;
+    private String photoURL;
+    private String name;
+    private String dob;
+    private float weight;
+    private String gender;
+
+
+    public NewDog() {
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public void setOwnerEmail(String ownerEmail) {
+        this.ownerEmail = ownerEmail;
+    }
+
+    public String getPhotoURL() {
+        return photoURL;
+    }
+
+    public void setPhotoURL(String photoURL) {
+        this.photoURL = photoURL;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "breed='" + breed + '\'' +
+                ", ownerEmail='" + ownerEmail + '\'' +
+                ", photoURL='" + photoURL + '\'' +
+                ", name='" + name + '\'' +
+                ", dob='" + dob + '\'' +
+                ", weight=" + weight +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
